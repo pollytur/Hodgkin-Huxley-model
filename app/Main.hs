00f2b0303289e65d678660874cee218171cc0b68
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-09-15 07:38:37 +0300

    Modify default code into a hello-world

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import           MyProject
 
 main :: IO ()
-main = someFunc
+main = run
---
+++ src/MyProject.hs
@@ -0,0 +1,6 @@
+{-# OPTIONS_GHC -Wall -fno-warn-type-defaults #-}
+{-# LANGUAGE OverloadedStrings #-}
+module MyProject where
+
+run :: IO ()
+run = putStrLn "Hello, world!"
